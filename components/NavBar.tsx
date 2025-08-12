33c304652abcc8260f988c91961a0b8f8ab32949
Grizix <prylipko.alexandr@gmail.com>
2025-08-12 23:01:42 +0300

    Fix

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,15 +3,17 @@ import { ConnectWallet } from "@thirdweb-dev/react";
 
 export default function NavBar() {
     return (
-        <Container maxW={"1200px"} py={4}>
-            <Flex direction={"row"} justifyContent={"space-between"}>
-                <Heading>Crypto Farm</Heading>
-                <Flex alignItems={"center"}>
-                    <Link href={"/"} mx={2}>Play</Link>
-                    <Link href={"/shop"} mx={2}>Shop</Link>
-                </Flex>
-                <ConnectWallet/>
-            </Flex>
-        </Container>
+      <Container maxW={"1200px"} py={4}>
+          <Flex direction={"row"} justifyContent={"space-between"}>
+              <Heading>Crypto Farm</Heading>
+              <Flex alignItems={"center"}>
+                  <Link href={"/"} mx={2}>Play</Link>
+                  <Link href={"/shop"} mx={2}>Shop</Link>
+              </Flex>
+              <ConnectWallet
+                switchToActiveChain={true}
+              />
+          </Flex>
+      </Container>
     )
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,43 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import { PolygonZkevmTestnet } from "@thirdweb-dev/chains";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import { defineChain } from "thirdweb";
 
 function MyApp({ Component, pageProps }: AppProps) {
   // Create QueryClient only once
   const [queryClient] = useState(() => new QueryClient());
 
+  const zenChain = defineChain({
+    id: 8408,
+    chainId: 8408,
+    rpc: "https://zenchain-testnet.api.onfinality.io/public",
+    nativeCurrency: {
+      name: "ZEN",
+      symbol: "ZEN",
+      decimals: 18
+    },
+    chain: "ZenChain Testnet",
+    name: "ZenChain Testnet",
+    shortName: "zen-testnet",
+    slug: "zenchain-testnet",
+    testnet: true
+  })
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThirdwebProvider
-        clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-        activeChain={{
-          chainId: 8408,
-          rpc: ["https://zenchain-testnet.api.onfinality.io/public"],
-          nativeCurrency: {
-            name: "ZEN",
-            symbol: "ZEN",
-            decimals: 18
-          },
-          name: "Zenchain Testnet",
-          shortName: "zen-testnet",
-          slug: "zenchain-testnet",
-        }}
-      >
+    <ThirdwebProvider
+      activeChain={zenChain}
+      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+    >
+      <QueryClientProvider client={queryClient}>
         <ChakraProvider value={defaultSystem}>
           <NavBar />
           <Component {...pageProps} />
         </ChakraProvider>
-      </ThirdwebProvider>
-    </QueryClientProvider>
+      </QueryClientProvider>
+    </ThirdwebProvider>
   );
 }
 
